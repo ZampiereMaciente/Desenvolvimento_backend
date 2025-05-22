6f9b63615c42ced1327e1e0257b809c217376560
R <renatozmlc@gmail.com>
2025-05-22 17:49:47 -0300

    Aula 15 - Oauth JWP

---
+++ produto/src/main/java/br/edu/ifmg/produto/projections/UserDetailsProjection.java
@@ -0,0 +1,14 @@
+package br.edu.ifmg.produto.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+
+    String getPassword();
+
+    Long getRoleId();
+
+    String getAuthority();
+
+
+}
--- produto/src/main/java/br/edu/ifmg/produto/services/UserService.java
+++ produto/src/main/java/br/edu/ifmg/produto/services/UserService.java
@@ -7,6 +7,7 @@ import br.edu.ifmg.produto.dtos.UserInsertDTO;
 import br.edu.ifmg.produto.entities.Product;
 import br.edu.ifmg.produto.entities.Role;
 import br.edu.ifmg.produto.entities.User;
+import br.edu.ifmg.produto.projections.UserDetailsProjection;
 import br.edu.ifmg.produto.repository.RoleRepository;
 import br.edu.ifmg.produto.repository.UserRepository;
 import br.edu.ifmg.produto.resources.ProductResource;
@@ -17,17 +18,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository repository;
@@ -95,12 +100,6 @@ public class UserService {
         }
     }
 
-
-
-
-
-
-
     private void copyDtoToEntity(UserDTO dto, User entity) {
         entity.setFirstName(dto.getFirstName());
         entity.setLastName(dto.getLastName());
@@ -113,4 +112,21 @@ public class UserService {
         }
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result
+                = repository.searchUserAndRoleByEmail(username);
+
+        if (result.isEmpty()){
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        User user = new User();
+        user.setEmail(result.get(0).getUsername());
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection p : result) {
+            user.addRole(new Role(p.getRoleId(), p.getAuthority()));
+        }
+        return user;
+    }
 }
